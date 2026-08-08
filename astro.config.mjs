76277000027fb885c1ab547050f6212c54d8791c
Nitanshu Vashistha <nitanshu.vzard@gmail.com>
2026-08-09 00:58:20 +0530

    Disable astro dev toolbar
    Signed-off-by: Nitanshu Vashistha <nitanshu.vzard@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  devToolbar: { enabled: false },
   fonts: [
     {
       name: 'Baloo 2',
